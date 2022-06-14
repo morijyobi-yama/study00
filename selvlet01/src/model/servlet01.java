92b7aa6096fee5cd927dcdab5cef886b3113e68e
morijyobi-yama <y.yama@morijyobi.ac.jp>
2022-06-14 09:23:33 +0900

    2回目

--- selvlet01/src/model/servlet01.java
+++ selvlet01/src/model/servlet01.java
@@ -1,6 +1,7 @@
 package model;
 
 import java.io.IOException;
+
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -11,10 +12,8 @@ import javax.servlet.http.HttpServletResponse;
  */
 public class servlet01 extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
+
+
     public servlet01() {
         super();
         // TODO Auto-generated constructor stub
@@ -28,9 +27,7 @@ public class servlet01 extends HttpServlet {
 		response.getWriter().append("Served at: ").append(request.getContextPath());
 	}
 
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-	 */
+
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		doGet(request, response);
